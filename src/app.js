158ed49050640db345d11c1cd2031c609c9f4388
purnananda <purna.behera@hotmail.com>
2022-11-18 10:13:58 +0530

    added my first class component

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import React from "react";
+import MyClassComponent from "./components/my-class-commponent";
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <MyClassComponent />
     </div>
   );
 }
---
+++ src/components/my-class-commponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class MyClassComponent extends React.Component {
+    render () {
+        return (
+            <h1>I am your Class Component. Cheers!!</h1>
+        )
+    }
+}
+
+export default MyClassComponent;
